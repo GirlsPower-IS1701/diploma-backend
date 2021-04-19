a2dc4efd5444a9041d3d2bb3f09f1cb8ef188201
Aru <aru.yelemes23@gmail.com>
2021-04-19 22:45:25 +0600

    Add DB

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,10 +10,10 @@ urlpatterns = [
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('api/register/', RegisterAPI.as_view(), name='register'),
     path('api/change-password/', ChangePasswordView.as_view(), name='change-password'),
-    path('api/logout/', views.LogoutView.as_view()),
+    # path('api/logout/', views.LogoutView.as_view()),
 
 
     # path('api/login/', LoginAPI.as_view(), name='login'),
-    # path('api/logout/', knox_views.LogoutView.as_view(), name='logout'),
+    path('api/logout/', knox_views.LogoutView.as_view(), name='logout'),
     # path('api/logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
 ]
