47b8bcf9d6ded49d1026c05619c350a1c786f765
Aru <aru.yelemes23@gmail.com>
2021-04-20 11:42:35 +0600

    reset password

--- diploma/settings.py
+++ diploma/settings.py
@@ -178,11 +178,11 @@ STATIC_URL = '/static/'
 
 ASGI_APPLICATION = 'diploma.wsgi.application'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'aru.elemes23@gmail.com'
 EMAIL_HOST_PASSWORD = '87071313236'
+EMAIL_USE_TLS = True
 
 
