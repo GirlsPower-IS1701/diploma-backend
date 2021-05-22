c60db02c1500d9a4b1cdb625bf19df93df4052e1
Aru <aru.yelemes23@gmail.com>
2021-05-22 11:44:33 +0600

    addserializer

--- timetable/serializers.py
+++ timetable/serializers.py
@@ -3,8 +3,10 @@ from rest_framework import serializers
 from accounts.serializers import UserSerializer
 from .models import Timetable
 
+from speciality_groups.serializers EnrollmentSerializer
 
 class TimetableSerialaizer(serializers.ModelSerializer):
+    enrollment = EnrollmentSerializer(read_only=True)
     class Meta:
         model = Timetable
-        fields = ('id', 'day_of_week', 'lesson_type', 'start_time', 'finish_time', 'room_number')
+        fields = ('id', 'day_of_week', 'lesson_type', 'enrollment', 'start_time', 'finish_time', 'room_number')
