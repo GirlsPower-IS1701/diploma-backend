b3e74d7c85cf90f34cdfb10927793de1796b275f
Aru <aru.yelemes23@gmail.com>
2021-05-24 14:42:46 +0600

    delete boto

--- diploma/settings.py
+++ diploma/settings.py
@@ -128,23 +128,23 @@ WSGI_APPLICATION = 'diploma.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'campus_mobile',
-        'USER': 'campus',
-        'PASSWORD': 'campus',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'campus_mobile',
+#         'USER': 'campus',
+#         'PASSWORD': 'campus',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
